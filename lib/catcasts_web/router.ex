c0abee1b834ea2277f9988cf88ebfa7921b7974e
Tonatiuh Núñez <tonatiuhnb@gmail.com>
2018-03-24 22:20:46 -0600

    add Ueberauth to permit authentication with Google

--- lib/catcasts_web/router.ex
+++ lib/catcasts_web/router.ex
@@ -19,6 +19,13 @@ defmodule CatcastsWeb.Router do
     get "/", PageController, :index
   end
 
+  scope "/auth", CatcastsWeb do
+    pipe_through :browser
+
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :new
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", CatcastsWeb do
   #   pipe_through :api
